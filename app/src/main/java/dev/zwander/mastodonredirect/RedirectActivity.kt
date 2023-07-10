a168596e2b72599b02bdb942b2fa264c605c34ba
Zachary Wander <zachary.wander@gmail.com>
2023-07-10 00:35:30 -0400

    Launch in new task

--- app/src/main/java/dev/zwander/mastodonredirect/RedirectActivity.kt
+++ app/src/main/java/dev/zwander/mastodonredirect/RedirectActivity.kt
@@ -1,5 +1,6 @@
 package dev.zwander.mastodonredirect
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -15,6 +16,8 @@ class RedirectActivity : ComponentActivity(), CoroutineScope by MainScope() {
 
         prefs.selectedApp.run {
             createIntents(url).forEach {
+                it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
                 try {
                     startActivity(it)
                 } catch (e: Exception) {

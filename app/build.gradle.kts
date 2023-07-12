65bcb625496a7686ef7b9425052ac06b869f43fd
Zachary Wander <zachary.wander@gmail.com>
2023-07-12 00:22:52 -0400

    Version 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.zwander.mastodonredirect"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

dbc3bb234a9ea79dd796bd9538bf5b1316f6692e
Zachary Wander <zachary.wander@gmail.com>
2023-07-20 00:01:00 -0400

    Remove unused import

--- app/src/main/java/dev/zwander/mastodonredirect/MainActivity.kt
+++ app/src/main/java/dev/zwander/mastodonredirect/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding

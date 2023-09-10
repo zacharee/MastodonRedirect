2f752b0ab258df9e003ae40d9444ea4f38fb0806
Zachary Wander <zachary.wander@gmail.com>
2023-09-10 17:12:41 -0400

    Move Shizuku link reset option to hidden dialog in all variants

--- shared/src/main/java/dev/zwander/shared/components/FooterLayout.kt
+++ shared/src/main/java/dev/zwander/shared/components/FooterLayout.kt
@@ -7,16 +7,14 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -27,16 +25,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.bugsnag.android.Bugsnag
-import dev.zwander.shared.BuildConfig
 import dev.zwander.shared.R
 import dev.zwander.shared.model.LocalAppModel
 import dev.zwander.shared.util.LinkVerificationModel
@@ -50,10 +45,13 @@ import tk.zwander.patreonsupportersretrieval.view.SupporterView
 private data class FooterButton(
     @StringRes val labelRes: Int,
     @DrawableRes val iconRes: Int,
+    val onLongClick: (() -> Unit)? = null,
     val onClick: () -> Unit,
 )
 
-@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
+@OptIn(
+    ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class,
+)
 @Composable
 fun FooterLayout(
     modifier: Modifier = Modifier,
@@ -64,6 +62,9 @@ fun FooterLayout(
         mutableStateOf(false)
     }
     var showingSupportersDialog by remember {
+        mutableStateOf(false)
+    }
+    var showingShizukuResetDialog by remember {
         mutableStateOf(false)
     }
 
@@ -96,6 +97,9 @@ fun FooterLayout(
             FooterButton(
                 R.string.options,
                 R.drawable.baseline_settings_24,
+                onLongClick = {
+                    showingShizukuResetDialog = true
+                },
             ) {
                 showingSettingsDialog = true
             },
@@ -110,8 +114,9 @@ fun FooterLayout(
             horizontalArrangement = Arrangement.SpaceEvenly,
         ) {
             buttons.forEach { button ->
-                IconButton(
+                LongClickableIconButton(
                     onClick = button.onClick,
+                    onLongClick = button.onLongClick,
                 ) {
                     Icon(
                         painter = painterResource(id = button.iconRes),
@@ -131,6 +136,12 @@ fun FooterLayout(
     if (showingSupportersDialog) {
         SupportersDialog {
             showingSupportersDialog = false
+        }
+    }
+
+    if (showingShizukuResetDialog) {
+        ShizukuResetDialog {
+            showingShizukuResetDialog = false
         }
     }
 }
@@ -165,7 +176,6 @@ private fun OptionsDialog(
     val context = LocalContext.current
     val appModel = LocalAppModel.current
     val prefs = appModel.prefs
-    val scope = rememberCoroutineScope()
 
     var enableCrashReports by prefs.enableCrashReports.rememberMutablePreferenceState()
     var openMediaInBrowser by prefs.openMediaInBrowser.rememberMutablePreferenceState()
@@ -208,40 +218,51 @@ private fun OptionsDialog(
                         modifier = Modifier.fillMaxWidth(),
                     )
                 }
-
-                if (BuildConfig.DEBUG) {
-                    item {
-                        AnimatedCard(
-                            onClick = {
-                                scope.launch(Dispatchers.IO) {
-                                    context.runShizukuCommand(Dispatchers.IO) {
-                                        unverifyLinks(Build.VERSION.SDK_INT, context.packageName)
-                                        LinkVerificationModel.refresh()
-                                    }
-                                }
-                            },
-                            modifier = Modifier.fillMaxWidth(),
-                        ) {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .heightIn(min = 48.dp),
-                                verticalAlignment = Alignment.CenterVertically,
-                            ) {
-                                Text(
-                                    text = stringResource(id = R.string.reset_link_verification),
-                                    textAlign = TextAlign.Center,
-                                    modifier = Modifier.fillMaxWidth(),
-                                )
-                            }
-                        }
-                    }
-                }
             }
         },
         confirmButton = {
             TextButton(onClick = onDismissRequest) {
                 Text(text = stringResource(id = android.R.string.ok))
+            }
+        },
+    )
+}
+
+@Composable
+private fun ShizukuResetDialog(
+    onDismissRequest: () -> Unit,
+) {
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    AlertDialog(
+        onDismissRequest = onDismissRequest,
+        title = {
+            Text(text = stringResource(id = R.string.reset_link_verification))
+        },
+        text = {
+            Text(text = stringResource(id = R.string.reset_link_verification_desc))
+        },
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text(text = stringResource(id = R.string.no))
+            }
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    scope.launch(Dispatchers.IO) {
+                        context.runShizukuCommand(Dispatchers.IO) {
+                            unverifyLinks(Build.VERSION.SDK_INT, context.packageName)
+                            LinkVerificationModel.refresh()
+                        }
+                    }
+                },
+            ) {
+                Text(
+                    text = stringResource(id = R.string.yes),
+                    color = MaterialTheme.colorScheme.error,
+                )
             }
         },
     )
---
+++ shared/src/main/java/dev/zwander/shared/components/LongClickableIconButton.kt
@@ -0,0 +1,58 @@
+@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE", "EXPOSED_PARAMETER_TYPE")
+package dev.zwander.shared.components
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.minimumInteractiveComponentSize
+import androidx.compose.material3.tokens.IconButtonTokens
+import androidx.compose.material3.value
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.semantics.Role
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun LongClickableIconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    onLongClick: (() -> Unit)? = null,
+    content: @Composable () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .minimumInteractiveComponentSize()
+            .size(IconButtonTokens.StateLayerSize)
+            .clip(IconButtonTokens.StateLayerShape.value)
+            .background(color = colors.containerColor(enabled).value)
+            .combinedClickable(
+                onClick = onClick,
+                enabled = enabled,
+                role = Role.Button,
+                interactionSource = interactionSource,
+                indication = rememberRipple(
+                    bounded = false,
+                    radius = IconButtonTokens.StateLayerSize / 2
+                ),
+                onLongClick = onLongClick,
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        val contentColor = colors.contentColor(enabled).value
+        CompositionLocalProvider(LocalContentColor provides contentColor, content = content)
+    }
+}

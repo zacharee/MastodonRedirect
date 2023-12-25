022ef60ef674ef19876813943f193b9677ec80ff
Zachary Wander <zachary.wander@gmail.com>
2023-12-25 10:29:39 -0500

    Update padding on SearchBar

--- shared/src/main/java/dev/zwander/shared/components/LinkBlocklistLayout.kt
+++ shared/src/main/java/dev/zwander/shared/components/LinkBlocklistLayout.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -97,7 +98,7 @@ fun LinkBlocklistLayout(
                                 listState.animateScrollToItem(0)
                             }
                         },
-                        modifier = Modifier.fillMaxWidth(),
+                        modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp),
                     )
                 }
             }

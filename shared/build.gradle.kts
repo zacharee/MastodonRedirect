b42cc48ddda9cc19f39085b9481cb67ba0eaf746
Zachary Wander <zachary.wander@gmail.com>
2025-08-06 17:30:14 -0400

    Remove unneeded suppression

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,5 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
